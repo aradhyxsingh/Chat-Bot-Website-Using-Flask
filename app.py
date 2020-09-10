a296b1eeeb4b47b267fb6b81750a98c280e475ff
aradhyxsingh <aradhyasingh0507@gmail.com>
2020-09-10 15:28:23 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run() 	
